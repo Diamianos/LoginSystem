88227d8f816b917b28b976fdf1e7de076fdb7593
Diamianos <70078183+Diamianos@users.noreply.github.com>
2020-10-25 22:00:55 -0400

    Updates

--- main.py
+++ main.py
@@ -9,65 +9,91 @@
 import stdiomask # Module to hide the characters when typing
 import sqlite3
 
-def main():
+conn = sqlite3.connect("Login DB.db") # Setting the conneciton to the database
+c = conn.cursor() # Creating the cursor to manipulate the database
 
-    conn = sqlite3.connect("Login DB.db") # Setting the conneciton to the database
-    c = conn.cursor() # Creating the cursor to manipulate the database
+def main():
 
     # Creating the database if it doesnt exist
     c.execute("CREATE TABLE IF NOT EXISTS usernameAndPasswords(username TEXT, password TEXT)")
 
     print("Welcome to my first login system.\n")
-    user_selection = str(input("Please either enter \"1\" to login or \"2\" to create an account: "))
+    print("Please choose from the following:" + 
+            "\n(1) Login" +
+            "\n(2) Create a new account")
+
+    print()
+    user_selection = str(input("-- "))
 
     # Input validation 
     while user_selection != "1" and user_selection != "2":
         user_selection = str(input("Please either enter \"1\" to login or \"2\" to create an account: "))
 
-    # Empty print statment for spacing
-    print()
-
     # If user entered "1" verify their login credentials and log them in
     if user_selection == "1":
-        
-        # Getting the user input
-        username = input("Username: ")
+        login()
+    elif user_selection == "2":
+        createNewUser()
+
+
+def login():
+    # Getting the user input
+    print("Please enter your username and password when prompted.")
+    username = input("Username: ").lower()
+    password = stdiomask.getpass("Password: ")
+
+    # Getting the username data based on the user name entered
+    c.execute(f'SELECT username FROM usernameAndPasswords WHERE username = "{username}"')
+    db_username = c.fetchone()
+
+    while db_username == None:
+        print("\nUsername not found please try again or create another account.")
+
+        # Asking for credentials again
+        username = input("Username: ").lower()
         password = stdiomask.getpass("Password: ")
 
-        # Getting the table based on the user name entered
+        # Getting the username data based on the user name entered
         c.execute(f'SELECT username FROM usernameAndPasswords WHERE username = "{username}"')
         db_username = c.fetchone()
 
-        # Getting the password associated to the username that was entered
-        c.execute(f'SELECT password FROM usernameAndPasswords WHERE username = "{username}"')
-        db_password = c.fetchone()
+    # Getting the password data associated to the username that was entered
+    c.execute(f'SELECT password FROM usernameAndPasswords WHERE username = "{username}"')
+    db_password = c.fetchone()
 
-        # Assigning the results to a variable and converting the data from a tuple to a string
-        db_username = ''.join(db_username)
-        db_password = ''.join(db_password)
+    # Assigning the results to a variable and converting the data from a tuple to a string
+    db_username = ''.join(db_username)
+    db_password = ''.join(db_password)
 
-        # If statement to determine if they got the correct username and password
-        if username == db_username and password == db_password:
-            print("You are logged in!")
+    # If statement to determine if they got the correct username and password
+    if username == db_username.lower() and password == db_password:
+        print("\nYou are logged in!")
 
-        else:
-            print("Something is wrong with the credentials.")
+    else:
+        print("\nSomething is wrong with the credentials, please try again")
 
-        c.close()
-        conn.close()
+    c.close()
+    conn.close()
 
-    # If user enters "2" create a new account, get info and write to a file
-    elif user_selection == "2":
+    
 
-        new_username = input("Please enter a new username: ")
-        new_password = stdiomask.getpass("Please enter a new password: ")
+def createNewUser():
+    new_username = input("Please enter a new username: ").lower()
+    new_password = stdiomask.getpass("Password: ")
+    repeat_password = stdiomask.getpass("Repeat password: ")
 
-        c.execute(f'INSERT INTO usernameAndPasswords VALUES("{new_username}", "{new_password}")')
-        print() # Empty print statment for spacing
-        print("Saving user name and password to the database.")
-        conn.commit()
-        c.close()
-        conn.close()
+    while new_password != repeat_password:
+        print()
+        print("Passwords did not match")
+        new_password = stdiomask.getpass("Password: ")
+        repeat_password = stdiomask.getpass("Repeat password: ")
+
+    c.execute(f'INSERT INTO usernameAndPasswords VALUES("{new_username}", "{new_password}")')
+    print() # Empty print statment for spacing
+    print("Saving user name and password to the database.")
+    conn.commit()
+    c.close()
+    conn.close()
 
 
 main()
